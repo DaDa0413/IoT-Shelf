b302bed011392a3f6b2e4f51c61076b692956156
DaAndLing <actf5795nb8@gmail.com>
2020-06-17 11:07:18 +0800

    First commit

---
+++ IoT.py
@@ -0,0 +1,63 @@
+import paho.mqtt.client as mqtt
+import json
+import time
+import sys
+
+from utils.shelf import shelf_module
+
+projectKey = 'PK7TPY1BSCAHCZ1E5H'
+deviceId = '22725254260'
+sensorId = 'openlid'
+
+host = "iot.cht.com.tw"
+topic = '/v1/device/' + deviceId +  '/sensor/' + sensorId + '/rawdata'
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code: {}".format(str(rc)))
+    client.subscribe(topic, 2)
+    
+def on_message(client, userdata, msg):
+    global mqtt_sub_enable
+    json_array = json.loads(str(msg.payload)[2:-1])
+    # Enable lid
+    if json_array['value'][0] == '1' and mqtt_sub_enable == True:
+        shelf.get_mqtt_handler()
+        mqtt_sub_enable = False
+
+if __name__ == '__main__':
+
+    # Set mqtt
+    mqtt_sub_enable = False
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    user, password = projectKey, projectKey
+    client.username_pw_set(user, password)
+    client.connect(host, 1883, 60)
+    client.loop_start()
+
+    shelf = shelf_module(client)
+    while(True):
+        if shelf.state == 'wait_for_mqtt':
+            if mqtt_sub_enable == False:
+                mqtt_sub_enable = True
+        elif shelf.state == 'wait_for_open':
+            # TODO Enable photoresistor
+            print('wait for open~~~')
+            time.sleep(1)
+            strs = input()
+            if strs == 'open':
+                shelf.open_lid()
+        elif shelf.state == 'wait_for_close':
+            # TODO Enable switch & timer
+            print('wait for close~~')
+            strs = input()
+            if strs == 'close':
+                shelf.close_lid()
+        elif shelf.state == 'forget_to_close':
+            # TODO Enable buzzer
+            pass
+        else:
+            print("[ERROR] Unknown state")
+            
+        
---
+++ utils/shelf.py
@@ -0,0 +1,27 @@
+from transitions import Machine
+class shelf_module(object):
+
+    states = ['wait_for_mqtt', 'wait_for_open', 'wait_for_close', 'forget_to_close']
+
+    def __init__(self, client):
+        self.client = client
+        self.machine = Machine(model=self, states=shelf_module.states, initial='wait_for_mqtt')
+        self.machine.add_transition(trigger='get_mqtt', source='wait_for_mqtt', dest='wait_for_open')
+        self.machine.add_transition(trigger='open_lid', source='wait_for_open', dest='wait_for_close')
+        self.machine.add_transition(trigger='close_lid', source='wait_for_close', dest='wait_for_mqtt')
+        self.machine.add_transition(trigger='open_too_long', source='wait_for_close', dest='forget_to_close')
+        self.machine.add_transition(trigger='close_lid', source='forget_to_close', dest='wait_for_mqtt')
+
+    def get_mqtt_handler(self):
+        # self.client.loop_stop()
+        self.get_mqtt()
+        print('State:' + self.state)
+    
+    def open_lid_handler(self):
+        pass
+    
+    def close_lid_handler(self):
+        pass
+    
+    def open_too_long_handler(self):
+        pass
